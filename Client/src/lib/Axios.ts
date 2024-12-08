c2849eadfa81f8093c2b42343dc685377709b457
M Subash <105859085+Git-Subash@users.noreply.github.com>
2024-12-08 21:03:44 +0530

    vercel.json-added-backend

--- Client/src/lib/Axios.ts
+++ Client/src/lib/Axios.ts
@@ -1,74 +1,3 @@
-// import { baseURL, SummaryApi } from "@/constants/SummaryApi";
-// import axios from "axios";
-
-// const Axios = axios.create({
-//   baseURL: baseURL,
-//   withCredentials: true,
-// });
-
-// //sending access token in the header
-// Axios.interceptors.request.use(
-//   async (config) => {
-//     const accessToken = localStorage.getItem("accessToken");
-
-//     if (accessToken) {
-//       config.headers.Authorization = `Bearer ${accessToken}`;
-//     }
-
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
-
-// //extend the life span of access token with
-// // the help refresh
-// Axios.interceptors.request.use(
-//   (response) => {
-//     return response;
-//   },
-//   async (error) => {
-//     let originRequest = error.config;
-
-//     if (error.response.status === 401 && !originRequest.retry) {
-//       originRequest.retry = true;
-
-//       const refreshToken = localStorage.getItem("refreshToken");
-
-//       if (refreshToken) {
-//         const newAccessToken = await refreshAccessToken(refreshToken);
-
-//         if (newAccessToken) {
-//           originRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-//           return Axios(originRequest);
-//         }
-//       }
-//     }
-
-//     return Promise.reject(error);
-//   }
-// );
-
-// const refreshAccessToken = async (refreshToken: string) => {
-//   try {
-//     const response = await Axios({
-//       ...SummaryApi.refreshToken,
-//       headers: {
-//         Authorization: `Bearer ${refreshToken}`,
-//       },
-//     });
-
-//     const accessToken = response.data.data.accessToken;
-//     localStorage.setItem("accessToken", accessToken);
-//     return accessToken;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// export default Axios;
-
 import { baseURL, SummaryApi } from "@/constants/SummaryApi";
 import axios from "axios";
 
@@ -90,7 +19,7 @@ Axios.interceptors.request.use(
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
 
 // Handle 401 Unauthorized error by refreshing the access token using the refresh token in cookies
@@ -126,7 +55,7 @@ Axios.interceptors.response.use(
 
     // If refresh fails or not a 401, reject the error
     return Promise.reject(error);
-  }
+  },
 );
 
 // Function to get a specific cookie by name
